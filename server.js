31f4d76272fd7d5b71490f5b543ace5186d07dba
Ahmadreza <a.joolaee@gmail.com>
2025-04-15 17:35:00 +0330

    refactor: convert server.js to ES6 module syntax

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const multer = require('multer');
-const { spawn } = require('child_process');
-const fs = require('fs');
-const path = require('path');
+import express from 'express';
+import multer from 'multer';
+import { spawn } from 'child_process';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
